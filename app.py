71a7196fbb92f29e3d2cd632198c389e529b8c34
SherjeelAli <sherjeelali7@gmail.com>
2026-09-14 21:25:39 +0500

    Final Changes

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ encoder = VGGEncoder("vgg_normalised.pth").to(device)
 decoder = Decoder().to(device)
 decoder.load_state_dict(
     torch.load(
-        r"C:\Users\HP\Desktop\NST\experiment\final_exp\decoder_final.pth",
+       "experiment/final_exp/decoder_final.pth",
         map_location=torch.device("cpu"),
     )
 )
